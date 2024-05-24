557e7cb58f53dfc88bdb92c00960e8eeb2d6a727
zcmgyu <zcmgyu@gmail.com>
2024-05-24 18:05:03 +0900

    feat: setup Tamagui@1.98.4

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
+process.env.TAMAGUI_TARGET = 'native';
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      '@tamagui/babel-plugin',
+      {
+        components: ['tamagui'],
+        config: './tamagui.config.ts',
+        logTimings: true,
+        disableExtraction: process.env.NODE_ENV === 'development',
+      },
+    ],
+    [
+      'transform-inline-environment-variables',
+      {
+        include: ['TAMAGUI_TARGET'],
+      },
+    ],
+  ],
 };
